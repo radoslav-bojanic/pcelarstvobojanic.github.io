d357fac1266f3a0f6f56f924841b3a5618a9fd6c
radoslav-bojanic <rasabozej2@gmail.com>
2024-10-09 21:18:15 +0200

    fixed issue in js top title mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,7 +39,7 @@
 			$(
 				'<div id="titleBar">' +
 					'<a href="#navPanel" class="toggle"></a>' +
-					'<span class="title">' + $('#logo').html() + '</span>' +
+					'<span class="title"> Pčelarstvo Bojanić</span>' +
 				'</div>'
 			)
 				.appendTo($body);
