f19c364a5ee92a4e9fc34364c1ad045b2df017cf
radoslav-bojanic <rasabozej2@gmail.com>
2024-10-11 01:07:27 +0200

    storage

--- assets/js/galerija.js
+++ assets/js/galerija.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-analytics.js";
-import { storage } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-storage.js";
+import { } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-storage.js";
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
@@ -16,4 +16,4 @@ import { storage } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-sto
   // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const storage = storage();
+const storage = firebase.storage();
